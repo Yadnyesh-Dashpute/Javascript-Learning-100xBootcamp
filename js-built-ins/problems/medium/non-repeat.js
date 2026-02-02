c27c30f8f31607af5ad696d14bd959685b53fbcd
Yanyesh-Dashpute <ysdashpute04@gmail.com>
2026-02-02 23:50:15 +0530

    Completed the Week2 Second Part assignment based on simple hard and medium problem attached with the test cases

---
+++ js-built-ins/problems/medium/non-repeat.js
@@ -0,0 +1,39 @@
+/*
+  Write a function `nonrepeat` which takes a string as input and returns the first non-repeating character in the string.
+
+  What is a non-repeating character?
+  - A character that appears only once in the entire string.
+
+  Example:
+  - Input: "abcab"
+  - Output: "c"
+
+  - Input: "aabbcc"
+  - Output: null
+
+  - Input: "abcdef"
+  - Output: "a"
+
+  - Input: ""
+  - Output: null
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-nonrepeat`
+*/
+function nonrepeat(str) {
+  // Your code here
+
+  let map = new Map();
+
+  for (let char of str) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+
+  for (let char of str) {
+    if (map.get(char) === 1) {
+      return char;
+    }
+  }
+  return null
+}
+module.exports = nonrepeat;
